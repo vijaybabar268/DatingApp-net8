043bef77826872bfa77e3c04cc52cf0fa1e95162
vijay babar <babarvm@gmail.com>
2024-06-30 20:53:57 +0530

    End of Section 2 - Angular 17

--- API/Program.cs
+++ API/Program.cs
@@ -8,8 +8,11 @@ builder.Services.AddDbContext<DatingDataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultCS"));
 });
+builder.Services.AddCors();
+
 
 // Configure the HTTP request pipeline.
 var app = builder.Build();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 app.Run();
